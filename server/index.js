c9695aa1b38bd026160b897ac4bf163f06977832
filipe-machado <ax-filipe@hotmail.com>
2021-02-24 20:46:36 -0300

    🐛 RAWG
    correção em rota

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import clearDatabaseRoute from "./routes/clearDatabaseRoute.js";
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://rawgax.netlify.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });

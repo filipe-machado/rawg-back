a8473a05fe8260c6ac91816f2cd63d96a976dda7
filipe-machado <ax-filipe@hotmail.com>
2021-02-24 20:56:26 -0300

    🐛 RAWG
    correção em rota

--- server/routes/gameRoutes.js
+++ server/routes/gameRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
+import cors from 'cors';
 import { getGameData } from "../controllers/gameController.js";
 
 const router = express.Router();
 
-router.get("/:id?/:apiPath?", getGameData);
+router.get("/:id?/:apiPath?", getGameData, cors());
 
 export default router;

ca49b2bdb6e61ab9eb0bc1b9e24fb70e239d2d11
filipe-machado <ax-filipe@hotmail.com>
2021-02-24 21:03:32 -0300

    🐛 RAWG
    correção em rota

--- server/index.js
+++ server/index.js
@@ -14,7 +14,9 @@ import clearDatabaseRoute from "./routes/clearDatabaseRoute.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 
 app.options('*', cors());
 
